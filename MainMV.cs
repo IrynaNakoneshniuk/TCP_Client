5473528afedf40a49a963d6db842181427b2c290
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-02-05 01:01:21 +0200

    Add project files.

---
+++ MainMV.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TCP_Client
+{
+    public class MainMV : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public MainMV()
+        {
+            GetSendMsg = new  SendGetMsgCommand(this);
+        }
+
+        private string _message;
+        private string outMsg;
+        public ICommand GetSendMsg { get; set; }
+        public string OutMsg
+        {
+            get
+            {
+                return outMsg;
+            }
+            set
+            {
+                outMsg = value;
+                OnPropertyChanged(nameof(OutMsg));
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+    }
+}
---
+++ SendGetMsgCommand.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TCP_Client;
+using TCPClient;
+
+namespace TCP_Client
+{
+    public class SendGetMsgCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private MainMV mv;
+        public SendGetMsgCommand(MainMV mv)
+        {
+            this.mv = mv;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object? parameter)
+        {
+            TCP client = new TCP("127.0.0.1",8080);
+            client.GetConnection();
+            Task.Run(async () => { await client.SendMsg(mv.OutMsg); });
+            mv.OutMsg= await client.GetMsg();
+        }
+    }
+}
